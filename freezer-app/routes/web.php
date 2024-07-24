5754041fd83abe7bb7cc5ae044ec195a648fb384
luissilvak <luis.silva@tcema.tc.br>
2024-07-24 08:12:13 -0300

    Alterando configurações do projeto Freezer Control

--- freezer-app/routes/web.php
+++ freezer-app/routes/web.php
@@ -6,4 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 })->name('frontend.home');
 
-Route::get('/')->name('login');
+Route::get('/customer-administration', function () {
+    return to_route('filament.auth.login');
+})->name('login');
